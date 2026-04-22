1b2ad07c427b87ee5643d138fe547de444c16b42
Renuka Dhanawat <118900960+RenukaDhanawat@users.noreply.github.com>
2026-04-22 14:18:12 +0530

    Update main.tf

--- terraform-ec2-jenkins-demo-main/main.tf
+++ terraform-ec2-jenkins-demo-main/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "jenkins_demo" {
   ami           = "ami-0f5ee92e2d63afc18" # Ubuntu (Mumbai region)
-  instance_type = "t3.small"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "Jenkins-Terraform-EC2"
